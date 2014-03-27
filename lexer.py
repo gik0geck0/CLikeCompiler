882a906da04cfe60b10d4170f32f9a5a32065c2f
Matt Buland <gik0geck0@gmail.com>
2014-03-27 00:04:16 -0600

    Updated the comments regex to ignore all the comments (don't spit out a token) and make some corrections to the cfg

--- lexer.py
+++ lexer.py
@@ -11,6 +11,10 @@ tokens = (
     'TIMES',
     'DIVIDE',
 
+    'LSHIFT',
+    'RSHIFT',
+
+    'EQUALSEQUALS',
     'EQUALS',
     'LTHAN',
     'GTHAN',
@@ -27,7 +31,9 @@ tokens = (
     'RETURN',
     'IF',
     'ELSE',
-    'SYMBOL',
+    'INT',
+    'CONST',
+    'ID',
 )
 
 # Regular expression rules for simple tokens
@@ -35,9 +41,12 @@ t_PLUS      = r'\+'
 t_MINUS     = r'-'
 t_TIMES     = r'\*'
 
-t_COMMENT   = r'//.*$'
+#t_COMMENT   = r'//.*$'
 t_DIVIDE    = r'/'
+t_LSHIFT     = r'<<'
+t_RSHIFT     = r'>>'
 
+t_EQUALSEQUALS  = r'=='
 t_EQUALS    = r'='
 t_LTHAN     = r'<'
 t_GTHAN     = r'>'
@@ -54,6 +63,8 @@ t_SEMICOL   = r';'
 t_IF        = r'if'
 t_ELSE      = r'else'
 t_RETURN    = r'return'
+t_INT       = r'int'
+t_CONST     = r'const'
 
 t_ID    = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
@@ -68,6 +79,10 @@ def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
+def t_COMMENT(t):
+    r'//.*'
+    pass
+
 # A string containing ignored characters (spaces and tabs)
 t_ignore  = ' \t'
 
@@ -80,7 +95,8 @@ def t_error(t):
 lexer = lex.lex()
 
 # Give it some data
-lexer.input(sys.stdin.read())
+datainput = sys.stdin.read()
+lexer.input(datainput)
 
 # Tokenize
 while True:
--- parser.py
+++ parser.py
@@ -49,7 +49,7 @@ def p_statements_empty(p):
 
 def p_statements_one(p):
     'statements : statement SEMICOL statements'
-    p[0] = p[2].append(p[1])
+    p[0] = p[3].append(p[1])
 
 
 def p_statement(p):
@@ -64,7 +64,7 @@ def p_returnstmt(p):
     p[0] = ('RETURN', p[2])
 
 def p_varassign(p):
-    'varassign : SYMBOL EQUALS expr'
+    'varassign : ID EQUALS expr'
     p[0] = ('ASSIGN', p[1], p[3])
 
 def p_vardecl(p):
@@ -72,9 +72,9 @@ def p_vardecl(p):
     p[0] = ('DECLARE', p[1], p[2])
 
 def p_ifstmt(p):
-    '''ifstmt : IF LPAREN boolexpr RPAREN LCURLY stmts RCURLY
-              | IF LPAREN boolexpr RPAREN LCURLY stmts RCURLY ELSE stmt SEMICOL
-              | IF LPAREN boolexpr RPAREN LCURLY stmts RCURLY ELSE LCURLY stmts RCURLY'''
+    '''ifstmt : IF LPAREN boolexpr RPAREN LCURLY statements RCURLY
+              | IF LPAREN boolexpr RPAREN LCURLY statements RCURLY ELSE statement SEMICOL
+              | IF LPAREN boolexpr RPAREN LCURLY statements RCURLY ELSE LCURLY statements RCURLY'''
     if len(p) > 8:
         # Has an else
         # The ternary-like part differentiates where the stmt/stmts come from
@@ -85,48 +85,87 @@ def p_ifstmt(p):
 
 def p_type(p):
     'type : qualifiers INT'
-    p[0]
+    p[0] = (p[1], p[2])
 
 def p_qualifiers(p):
     '''qualifiers : qualifier
                   | qualifier qualifiers'''
+    if len(p) > 2:
+        # second rule: more qualifiers following
+        p[0] = p[2].append(p[1])
+    else:
+        p[0] = [p[1]]
 
 def p_qualifier(p):
     'qualifier : CONST'
+    p[0] = p[1]
+
+def p_qualifier_epsilon(p):
+    'qualifier : epsilon'
+    pass
 
 def p_vars(p):
     '''vars : ID EQUALS expr
             | ID COMMA vars
             | ID
             | ID EQUALS expr COMMA vars'''
+    if len(p) == 2:
+        # Just an ID
+        p[0] = [p[1]]
+    elif p[2] == 'EQUALS':
+        # we are definitely assigning the ID
+        if len(p) > 4:
+            p[0] = p[5].append(('INSTANTIATE', p[1], p[3]))
+        else:
+            p[0] = [('INSTANTIATE', p[1], p[3])]
+    #elif p[2] == 'COMMA':
+    else:
+        p[0] = p[3].append(p[1])
 
 def p_boolexpr(p):
     '''boolexpr : expr LTHAN expr
                 | expr GTHAN expr
-                | expr EQUALS EQUALS expr'''
+                | expr EQUALSEQUALS expr'''
+    p[0] = (p[2], p[0], p[3])
 
 def p_expr(p):
     '''expr : term
             | term PLUS term
             | term MINUS term'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = (p[2], p[1], p[3])
 
 def p_term(p):
     '''term : part
             | part TIMES part
-            | part DIV part
+            | part DIVIDE part
             | part LSHIFT part
             | part RSHIFT part'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = (p[2], p[1], p[3])
 
 def p_part(p):
     '''part : ID
             | NUM
             | LPAREN expr RPAREN'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = p[2]
+
+# Represents an epsilon / lambda / empty string / etc
+def p_epsilon(p):
+    'epsilon : '
 
 def p_error(p):
     print("Syntax error at '%s'" % p.value)
 
 yacc.yacc()
-parseTree = yacc.parse(mylexer.data)
+parseTree = yacc.parse(lexer.datainput)
 
 print(parseTree)
 
