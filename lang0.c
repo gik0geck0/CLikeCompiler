5024cfd3d3f4f52f5662719006047ed33d98f3fd
Matt Buland <gik0geck0@gmail.com>
2014-03-28 10:45:27 -0600

    Fix dumb semicolon mistake

--- lang0.c
+++ lang0.c
@@ -4,11 +4,11 @@ int x = 1 << a;
 int y, z = 3;
 
 y = z - x;
+
 if ( y <= 0 ) {
     z = (x+2) + z*z ;
+} else {
+    z = z / y;
 }
-// else {
-//    z = z / y;
-// }
-// 
-// return z;
+
+return z;
